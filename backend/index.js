af0487fa484e6ffed97661cf1846c41cda515126
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-05-11 14:07:19 +0600

    feat: almost todo created

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 // body{
 // title: string
--- fronted/src/components/CreateTodo.jsx
+++ fronted/src/components/CreateTodo.jsx
@@ -1,19 +1,48 @@
-export function CreateTodo() {
+import { useState } from "react";
+
+export function CreateTodo(props) {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
     return <div>
         <input
             style={{
                 padding: 5,
                 margin: 5
             }}
-            type="text" placeholder="title"></input><br></br>
+            type="text" placeholder="title" onChange={function (e) {
+                const value = e.target.value;
+                setTitle(e.target.value)
+            }}></input><br></br>
         <input style={{
             padding: 5,
             margin: 5
-        }} type="text" placeholder="description"></input> <br></br>
+        }} type="text" placeholder="description" onChange={function (e) {
+            const value = e.target.value
+            setDescription(e.target.value)
+        }}></input> <br></br>
 
         <button style={{
             padding: 5,
             margin: 5
-        }}>Add a todo</button>
+        }} onClick={() => {
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+
+            })
+                .then(async function (res) {
+                    const json = await res.json();
+                    alert("Todo Added")
+                })
+        }}
+        >Add a todo</button>
     </div>
 }
